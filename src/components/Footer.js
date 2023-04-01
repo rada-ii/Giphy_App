a2fc60992f89f5580336ea1a28f555fa282d8b76
rada-i <neposebanmail@gmail.com>
2023-04-01 22:16:52 +0200

    corrections

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer>
-      <div className="foot">
+    <footer className="foot">
+      <div className="footer">
         <p>&copy; BIT 2023 By Radmila Ivankovic</p>
       </div>
     </footer>
